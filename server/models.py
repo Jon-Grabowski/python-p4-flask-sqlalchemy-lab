796ad9371f8b02325be3561f1cb3182451961e51
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-07-25 23:16:09 -0400

    All tests passing

--- server/models.py
+++ server/models.py
@@ -7,17 +7,34 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+# The Zookeeper model should contain a name, a birthday, and a list of animals that they take care of.
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
 
+    animals = db.relationship('Animal', backref = 'zookeeper')
+
+# The Enclosure model should contain an environment (grass, sand, or water), an open_to_visitors boolean, and a list of animals.
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
 
+    animals = db.relationship('Animal', backref = 'enclosure')
+
+# The Animal model should contain a name, a species, a zookeeper, and an enclosure.
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+ 
